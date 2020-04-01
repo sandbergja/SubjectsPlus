1a27ecda07702c2b85c4393b7afc4f737fc22519
Jane Sandberg <sandbej@linnbenton.edu>
2020-03-31 18:52:25 -0700

    Updates to the QP chat widget

--- lib/SubjectsPlus/Control/Pluslet/views/QPChatViewOutput.php
+++ lib/SubjectsPlus/Control/Pluslet/views/QPChatViewOutput.php
@@ -1,2 +1,8 @@
-<div id="questionpoint.chatwidget1" qwidgetno="1" ></div>
-<script id="questionpoint.bootstrap1" qwidgetno="1" type="text/javascript" src="https://www.questionpoint.org/crs/qwidgetV4/js/qwidget.bootstrapnj.js?langcode=<?php echo $this->_lang_id; ?>&instid=<?php echo $this->_inst_id; ?>&skin=<?php echo $this->_color; ?>&size=fill" charset="utf-8">//<noscript>Please enable javascript to chat with librarians online</noscript></script> 
+<?php $qp_widget_id = rand(1,5); ?>
+<div id="questionpoint.chatwidget<?php echo $qp_widget_id; ?>"
+  qwidgetno="<?php echo $qp_widget_id; ?>" >
+</div>
+<script id="questionpoint.bootstrap<?php echo $qp_widget_id; ?>"
+  qwidgetno="<?php echo $qp_widget_id; ?>"
+  type="text/javascript"
+  src="https://www.questionpoint.org/crs/qwidgetV4/js/qwidget.bootstrapnj.js?langcode=<?php echo $this->_lang_id; ?>&instid=<?php echo $this->_inst_id; ?>&skin=<?php echo $this->_color; ?>&size=standard" charset="utf-8">//<noscript>Please enable javascript to chat with librarians online</noscript></script> 
