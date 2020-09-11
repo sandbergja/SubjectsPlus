e00178f29a96a11a3b2b91198015615a255fdbc5
Jane Sandberg <sandbergja@users.noreply.github.com>
2020-09-11 09:26:07 -0700

    Updating contact pluslet for Fall term

--- lib/SubjectsPlus/Control/Pluslet/views/ContactUsViewOutput.php
+++ lib/SubjectsPlus/Control/Pluslet/views/ContactUsViewOutput.php
@@ -1,6 +1,6 @@
 <?php
 $orgs = [
-  'hoc' => ['title' => 'HOC Library', 'phone' => '15419188840', 'formatted_phone' => '541-918-8840', 'email' => 'sobotkc@linnbenton.edu'],
+  'hoc' => ['title' => 'HOC Library', 'phone' => '15419188840', 'formatted_phone' => '541-918-8840', 'email' => 'miyagib@linnbenton.edu'],
   'circ' => ['title' => 'LBCC Library Circulation Services', 'phone' => '15419174638', 'formatted_phone' => '541-917-4638', 'email' => 'libcirc@linnbenton.edu'],
   'ref' => ['title' => 'LBCC Library Reference Services', 'phone' => '15419174645', 'formatted_phone' => '541-917-4645', 'email' => 'libref@linnbenton.edu'],
   'shd' => ['title' => 'Student Help Desk', 'phone' => '15419174630', 'formatted_phone' => '541-917-4630', 'email' => 'student.helpdesk@linnbenton.edu'],
@@ -24,7 +24,7 @@ if ($this->_contact_type) {
     Text: 541-704-7001
   </a>
 <?php else: ?>
-<a href="https://www.questionpoint.org/crs/servlet/org.oclc.admin.BuildForm?&page=frame&institution=13983&type=2&language=1" class="contact-pluslet-link">
+<a href="https://v2.libanswers.com/chati.php?hash=608b00394095f20b06021523d15418ff" class="contact-pluslet-link">
   <span class="fa fa-commenting fa-2x yellow-background contact-circle"></span>
   Chat
 </a>
